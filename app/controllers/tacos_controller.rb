b434ece4bd60da58a332428bf583f8e3cee1cb4d
Karina Finn <karina.finn@kellogg.northwestern.edu>
2024-02-06 17:04:42 +0000

    wip

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,6 +1,7 @@
 class TacosController < ApplicationController
 
   def index 
-    render :inline => "<h1>Tacos!!!</h1>"
+   # render :inline => "<h1>Tacos!!!</h1><p>I am hungry </p>"
+   render :template => "tacos/index"
   end 
 end
